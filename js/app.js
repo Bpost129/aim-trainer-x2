407195ab53c4cf6a7b590397e037d9669d0e8156
Bpost129 <Bpost129@yahoo.com>
2025-12-04 22:54:21 -0500

    add element references and event listeners to buttons

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const startBtn = document.getElementById('start-btn')
+const resetBtn = document.getElementById('reset-btn')
+
+startBtn.addEventListener('click', startGame)
+resetBtn.addEventListener('click', resetGame)
+
+function startGame() {
+
+}
+
+function resetGame() {
+  
+}
