532996b5ca4671c96315729e490c076497290598
Bpost129 <Bpost129@yahoo.com>
2025-12-04 23:43:46 -0500

    create a start game function to initiate timer

--- js/app.js
+++ js/app.js
@@ -1,13 +1,30 @@
+let timerIntervalId
+let winTime, min, sec, seconds = 0
+
 const startBtn = document.getElementById('start-btn')
 const resetBtn = document.getElementById('reset-btn')
+const timerEl = document.getElementById('timer')
 
 startBtn.addEventListener('click', startGame)
 resetBtn.addEventListener('click', resetGame)
 
 function startGame() {
+  if (timerIntervalId) {
+    seconds = 0
+    clearInterval(timerIntervalId)
+  }
+  renderTime()
+  timerIntervalId = setInterval(tick, 1000)
+}
+
+function tick() {
+
+}
+
+function renderTime() {
 
 }
 
 function resetGame() {
-  
+
 }
